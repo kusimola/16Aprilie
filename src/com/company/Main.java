eb706c3fa157d8f4ca30b07867f822e10ee2bace
imola <hjkk@yahoo.com>
2022-05-14 09:53:46 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("What is your name?");
+        Scanner sc =new Scanner(System.in);
+        String yourName = sc.nextLine();
+        System.out.println("Hello " + yourName + "!");
+
+        System.out.println("What is your favorite number?");
+        Scanner int
+    }
+}
